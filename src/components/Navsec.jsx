ab26abc52bf1e437d9a340ecef7ecd9e1021f30d
Your Name <you@example.com>
2024-04-05 09:49:05 +0530

    commit-update

--- src/components/Navsec.jsx
+++ src/components/Navsec.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import logo from '../assets/images/png/logo.png'
+import logo from '../assets/images/webp/logo.webp'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Commonbtn from './common/Commonbtn';
 const Navsec = () => {
